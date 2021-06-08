d1ea40f0b8a9b5502ce7ee3401be9b72f228a9db
Juliana Sierra <juliana.sierra@fabernovel.com>
2021-06-08 11:08:08 +0200

    Add documentation

--- tchat/commun/verificationFormat.ts
+++ tchat/commun/verificationFormat.ts
@@ -2,28 +2,41 @@ import * as t from 'io-ts';
 
 import {Mois, Semaine} from "../../bibliotheque/types/date";
 
-
+/**
+ * Schema d'un identifiant du sorte sommet à valider
+ */
 const IdentifiantSommet = t.type({
     val: t.string,
     sorte: t.literal("sommet")
 });
 
+/**
+ * Schema d'un identifiant du sorte message à valider
+ */
 const IdentifiantMessage = t.type({
     val: t.string,
     sorte: t.literal("message")
 });
 
+/**
+ * Schema d'un Tableau à valider
+ */
 const FormatTableau = t.type({
     taille: t.number,
     tableau: t.array(IdentifiantSommet)
 })
-
+/**
+ * Schema d'un message d'envoi à valider
+ */
 const FormatMessageEnvoiTchatValidator = t.type({
     ID_emetteur: IdentifiantSommet,
     contenu: t.string,
     ID_destinataires: FormatTableau
 })
 
+/**
+ * Union de tous les jours de la semaine à partir de l'enum Semaine
+ */
 const SemaineFormat = t.union([
     t.literal(Semaine.LUNDI),
     t.literal(Semaine.MARDI),
@@ -34,6 +47,9 @@ const SemaineFormat = t.union([
     t.literal(Semaine.DIMANCHE)
 ])
 
+/**
+ * Union de tous les mois à partir de l'enum Mois
+ */
 const MoisFormat = t.union([
     t.literal(Mois.JANVIER),
     t.literal(Mois.FEVRIER),
@@ -50,7 +66,7 @@ const MoisFormat = t.union([
 ])
 
 /**
- * Schéma JSON pour les dates et les heures francaises.
+ * Schema d'une date aà valider
  */
 const FormatDateValidator = t.type({
     seconde: t.number,
@@ -62,6 +78,9 @@ const FormatDateValidator = t.type({
     annee: t.number,
 })
 
+/**
+ * Schema d'un message provenant du client de type envoi
+ */
 export const FormatMessageTchatValidator = t.type({
     ID: IdentifiantMessage,
     type: t.literal("envoi"),
