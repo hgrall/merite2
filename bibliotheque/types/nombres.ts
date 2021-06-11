fd5bd11a19ceed7cd48e3330feedf5123253f110
hgrall <herve.grall@imt-atlantique.fr>
2021-06-11 16:28:30 +0200

    maj gitignore + nombre: ajout d'un param (base, 10 par defaut)

--- bibliotheque/types/nombres.ts
+++ bibliotheque/types/nombres.ts
@@ -2,10 +2,11 @@
  * Représentation d'un entier n en complétant à gauche avec des 0 si besoin.
  * @param n entier à représenter.
  * @param taille taille minimale à obtenir pour la représentation.
- * @returns une représentation en base 10 de l'entier n, avec au moins taille chiffres.
+ * @param base base utilisée pour la représentation
+ * @returns une représentation en base 'base' de l'entier n, avec au moins 'taille' chiffres.
  */
-export function normalisationNombre(n : number, taille : number) : string {
-    let r = n.toString();
+export function normalisationNombre(n : number, taille : number, base : number = 10) : string {
+    let r = n.toString(base);
     while(r.length < taille){
         r = "0" + r;
     }
